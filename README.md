fadbbf1fb4def662429e49e1d263c062570ed28d
Gustavo Bissolli <gustavo.bissolli@gmail.com>
2019-01-14 01:24:35 +0400

    Resolve merge

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# jquery-counter-up
-A jQuery plugin to count up a number with start on element visible feature
+# jQuery Counter Up
+A jQuery plugin to count up a number with start on element visible feature - [DOCS](https://bissolli.github.io/jquery-counter-up/)
+
+[![npm](https://img.shields.io/npm/v/jquery-counter-up.svg)](https://www.npmjs.com/package/jquery-counter-up)
+[![npm](https://img.shields.io/npm/dt/jquery-counter-up.svg)](https://www.npmjs.com/package/jquery-counter-up)
+[![npm](https://img.shields.io/npm/l/jquery-counter-up.svg)](https://github.com/bissolli/jquery-counter-up/blob/master/LICENSE)
+[![GitHub stars](https://img.shields.io/github/stars/bissolli/jquery-counter-up.svg)](https://github.com/bissolli/jquery-counter-up/stargazers)
+[![gzip size](http://img.badgesize.io/https://unpkg.com/jquery-counter-up?compression=gzip)](https://github.com/bissolli/jquery-counter-up)
+
+## Getting Started
+
+### 1. Install package using NPM or download it from the repository;
+```bash
+npm install jquery-counter-up --save
+```
+
+### 2. Require [jQuery](https://jquery.com/) and Counter-Up
+```html
+<script src="jquery.min.js"></script>
+<script src="jquery.counterup.min.js"></script>
+```
+or
+```javascript
+require('jquery');
+require('jquery-counter-up');
+```
+
+### 4. Add data attributes
+```html
+<span data-counter data-offset="100" data-duration="1000" data-delay="1000">400</span>
+```
+Through data properties you can set the following parameters:
+| Prop | Type | Default | Note |
+|---|---|---|---|
+| `offset` | `String` | 200 | Element top offset (in pixels) based on view top offset to start the animation
+| `duration` | `String` | 3000 | Time (in milliseconds) to count it up
+| `delay` | `String` | 200 | Delay (in milliseconds) to start the animation as per the element becomes visible
+
+### 5. Call Revealer
+```javascript
+$('[data-counter]').counterUp();
+```
+
+
+## JavaScript Options
+```javascript
+$('[data-counter]').counterUp({
+    decimals: 0,
+    decPoint: ".",
+    thousandsSep: ","
+});
+```
+
+
+## Methods
+```javascript
+$('[data-counter]').counterUp('bind');
+```
---
+++ src/jquery.counterup.js
@@ -0,0 +1,98 @@
+(function ($) {
+
+    var _elements,
+        _options;
+
+    var methods = {
+        init: function (elements, options) {
+            _elements = elements;
+            _options = options;
+
+            methods.bind();
+        },
+        bind: function () {
+            $(window).scroll(function () {
+                methods.animate(_elements, _options);
+            });
+            $(window).trigger('scroll');
+        },
+        animate: function (elements, options) {
+
+            var viewHeight = $(window).height(),
+                viewTopOffset = $(window).scrollTop(),
+                viewBottomOffset = (viewTopOffset + viewHeight);
+
+            $.each(elements, function (index) {
+                try {
+                    var elementTopOffset = $(this).offset().top;
+                } catch (e) {
+                    return;
+                }
+
+                var $this = $(this),
+                    elementOffset = $this.data('offset') || 200,
+                    elementDuration = $this.data('duration') || 3000,
+                    elementDelay = $this.data('delay') || 200,
+                    elementHeight = $this.outerHeight(),
+                    elementBottomOffset = (elementTopOffset + elementHeight);
+
+                if (elementOffset) {
+                    elementTopOffset += elementOffset;
+                    elementBottomOffset -= elementOffset;
+                }
+
+                if (parseInt($this.text()) > 0) {
+                    $this.attr('data-count-to', parseInt($this.text()))
+                }
+
+                var elementCountTo = parseInt($this.attr('data-count-to')) || 0;
+
+                $this.text(0);
+
+                if ((elementBottomOffset >= viewTopOffset) && (elementTopOffset <= viewBottomOffset)) {
+                    _elements.splice(index, 1);
+
+                    setTimeout(function () {
+                        $this.animate({ counter: elementCountTo }, {
+                            duration: elementDuration,
+                            easing: 'linear',
+                            step: function() {
+                                $this.text(methods.formatNumber(Math.floor(this.counter), _options.decimals, _options.decPoint, _options.thousandsSep));
+                            },
+                            complete: function() {
+                                $this.text(methods.formatNumber(Math.floor(this.counter), _options.decimals, _options.decPoint, _options.thousandsSep));
+                            }
+                        });
+                    }, elementDelay)
+                }
+            });
+        },
+        formatNumber: function (n, c, w, t) {
+            var c = isNaN(c = Math.abs(c)) ? 2 : c,
+                d = d == undefined ? "." : d,
+                t = t == undefined ? "," : t,
+                s = n < 0 ? "-" : "",
+                i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+                j = (j = i.length) > 3 ? j % 3 : 0;
+
+            return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+        }
+    };
+
+    jQuery.fn.counterUp = function (options) {
+        options = $.extend({
+            decimals: 0,
+            decPoint: ".",
+            thousandsSep: ","
+        }, options);
+
+        if (
+            options.mobile === false
+            && /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+        ) {
+            return false;
+        }
+
+        methods.init(this, options);
+    };
+})(jQuery);
